55649ef930a2bc3f1d40a205c5250003e2550c91
Jonas Dourado <jonas.jonaias@gmail.com>
2011-10-06 16:12:53 -0300

    All activity classes added

---
+++ src/org/AndroidShareApp/PersonListActivity.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package org.AndroidShareApp;
+
+import android.app.Activity;
+
+/**
+ * @author jonaias
+ *
+ */
+public class PersonListActivity extends Activity {
+
+}
---
+++ src/org/AndroidShareApp/SharedByMeConfigActivity.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package org.AndroidShareApp;
+
+import android.app.Activity;
+
+/**
+ * @author jonaias
+ *
+ */
+public class SharedByMeConfigActivity extends Activity {
+
+}
