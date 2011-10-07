604989ac14c807f038951c0a8d3b13479f11c9e7
Jonas Dourado <jonas.jonaias@gmail.com>
2011-10-07 01:04:59 -0300

    Main tabs implemented

--- src/org/AndroidShareApp/MainActivity.java
+++ src/org/AndroidShareApp/MainActivity.java
@@ -1,18 +1,37 @@
 package org.AndroidShareApp;
 
-import android.app.Activity;
+import android.app.TabActivity;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.widget.TabHost;
 
-public class MainActivity extends Activity {
+public class MainActivity extends TabActivity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.main_activity);
         
-        Intent mIntent = new Intent(MainActivity.this,SharedWithMeListActivity.class);
-        MainActivity.this.startActivity(mIntent);
+        Resources res = getResources(); // Resource object to get Drawables
+        TabHost tabHost = getTabHost();  // The activity TabHost
+        Intent intent;  // Reusable Intent for each tab
+
+        // Add a new tab with SharedByMeActivity
+        intent = new Intent(this,SharedWithMeListActivity.class);
+        tabHost.addTab(tabHost.newTabSpec("sharedByMe").setIndicator("SharedByMe")
+                      .setContent(intent));
+
+        //  Add a new tab with SharedWithMeActivity
+        intent = new Intent(this,SharedWithMeListActivity.class);
+        tabHost.addTab(tabHost.newTabSpec("sharedWithMe").setIndicator("SharedWithMe")
+        						  .setContent(intent));
+        
+        for (int i = 0; i < tabHost.getTabWidget().getTabCount(); i++) {
+            tabHost.getTabWidget().getChildAt(i).getLayoutParams().height = 23;
+        }
+
+        tabHost.setCurrentTab(1);
     }
 
     
