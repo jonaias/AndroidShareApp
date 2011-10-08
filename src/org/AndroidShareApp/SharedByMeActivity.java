74d40097125a017272b6f2f38eb2b3fbe1478c69
Andre Cunha <andrecunha.usp@gmail.com>
2011-10-08 19:05:18 -0300

    Added copyright information.

--- src/org/AndroidShareApp/SharedByMeActivity.java
+++ src/org/AndroidShareApp/SharedByMeActivity.java
@@ -21,10 +21,31 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-/**
- * @author Andre Luiz Verucci da Cunha
+/*
+ * Copyright (C) 2009 codemobiles.com.
+ * http://www.codemobiles.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * author: Chaiyasit Tayabovorn 
+ * email: chaiyasit.t@gmail.com
+ * 
+ * Modified in October 2011 for this project by:
+ * author: Andre Luiz Verucci da Cunha
+ * e-mail: andrecunha.usp@gmail.com
  * 
  */
+
 public class SharedByMeActivity extends ListActivity {
 
 	private EfficientAdapter adap;
