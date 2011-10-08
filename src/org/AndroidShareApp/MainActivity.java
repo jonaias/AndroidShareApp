83c25d3429a1cd95ee86da4d1eef984ee40d9408
Andre Cunha <andrecunha.usp@gmail.com>
2011-10-08 18:50:25 -0300

    Added SharedWithMeActivity, along with necessary resources.

--- src/org/AndroidShareApp/MainActivity.java
+++ src/org/AndroidShareApp/MainActivity.java
@@ -18,18 +18,18 @@ public class MainActivity extends TabActivity {
         Intent intent;  // Reusable Intent for each tab
 
         // Add a new tab with SharedByMeActivity
-        intent = new Intent(this,SharedWithMeListActivity.class);
-        tabHost.addTab(tabHost.newTabSpec("sharedByMe").setIndicator("SharedByMe")
+        intent = new Intent(this,SharedByMeActivity.class);
+        tabHost.addTab(tabHost.newTabSpec("sharedByMe").setIndicator("Shared By Me")
                       .setContent(intent));
 
         //  Add a new tab with SharedWithMeActivity
         intent = new Intent(this,SharedWithMeListActivity.class);
-        tabHost.addTab(tabHost.newTabSpec("sharedWithMe").setIndicator("SharedWithMe")
+        tabHost.addTab(tabHost.newTabSpec("sharedWithMe").setIndicator("Shared With Me")
         						  .setContent(intent));
         
-        for (int i = 0; i < tabHost.getTabWidget().getTabCount(); i++) {
-            tabHost.getTabWidget().getChildAt(i).getLayoutParams().height = 23;
-        }
+        //for (int i = 0; i < tabHost.getTabWidget().getTabCount(); i++) {
+         //   tabHost.getTabWidget().getChildAt(i).getLayoutParams().height = 23;
+        //}
 
         tabHost.setCurrentTab(1);
     }
