4f2cb83d0cb8951c917d7fbb7dfb84f871507a32
Jonas Dourado <jonas.jonaias@gmail.com>
2011-10-27 01:06:50 -0200

    NetworkSender is sending

--- src/org/AndroidShareApp/core/NetworkSender.java
+++ src/org/AndroidShareApp/core/NetworkSender.java
@@ -10,38 +10,36 @@ import java.net.UnknownHostException;
 import android.os.Handler;
 
 public class NetworkSender extends Thread {
-	Handler mHandler;
+	//Handler mHandler;
 	
 	public NetworkSender(){
-		mHandler = new Handler();
+		//mHandler = new Handler();
 	}
 	
 	@Override
 	public void run() {
 		/* Executes it every 5 s */
-		mHandler.postDelayed(this, 5000);
-		
-		DatagramSocket socket;
-		try {
-			byte[] message = new byte[100];
-			message = "Oh Hai!".getBytes();
-			
-			socket = new DatagramSocket();
-			socket.setBroadcast(true);
-			DatagramPacket packet = new DatagramPacket(message,100 ,
-					InetAddress.getByName("127.0.0.1"), 9764);
-			socket.send(packet);
-		} catch (SocketException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (UnknownHostException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		//mHandler.postDelayed(this, 5000);
+		while(!isInterrupted()){
+			DatagramSocket socket;
+			try {
+				byte[] message = "Oh Hai!".getBytes();
+				socket = new DatagramSocket(9763, InetAddress.getByName("10.0.2.15"));
+				//socket.setBroadcast(true);
+				DatagramPacket packet = new DatagramPacket(message, message.length ,
+						InetAddress.getByName("10.0.2.2"), 9764);
+				socket.send(packet);
+			} catch (SocketException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (UnknownHostException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
-		
 		
 	}
 
